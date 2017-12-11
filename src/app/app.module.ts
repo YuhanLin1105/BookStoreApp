2ecfccf132605a141c18d020d1c1cb6cc80b3a7e
Abhilash Reddy <abhilash.reddy@outlook.com>
2017-12-11 15:22:59 -0500

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SiteNavbarComponent } from './site-navbar/site-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminBooksComponent } from './admin/admin-books/admin-books.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiteNavbarComponent
+    SiteNavbarComponent,
+    HomeComponent,
+    BooksComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminBooksComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule
