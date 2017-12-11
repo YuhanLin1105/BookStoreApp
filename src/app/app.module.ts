49a440d20e376fea2d6d83e8947b3347fd2b0f1d
Abhilash Reddy <abhilash.reddy@outlook.com>
2017-12-11 15:16:09 -0500

    Installed Bootstarp and created navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SiteNavbarComponent } from './site-navbar/site-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteNavbarComponent
   ],
   imports: [
     BrowserModule
